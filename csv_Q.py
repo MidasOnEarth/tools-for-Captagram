4c5283c001f9560d0178be87c420e8cf7f7275e2
MidasOnEarth <kbatavia12@gmail.com>
2019-06-15 12:57:03 +0530

    added with only q

---
+++ csv_Q.py
@@ -0,0 +1,51 @@
+'''
+CSV creator
+'''
+
+import pprint
+import os
+os.system('cls')
+
+########################################
+ID=1156	# last ID + 1 
+MAIN_TAG='love'
+########################################
+
+f=open(MAIN_TAG+'.log','r')
+ff=f.readlines()
+pp = pprint.PrettyPrinter(indent=4)
+# pp.pprint(ff)
+ff=[s.strip('\n') for s in ff]
+
+captions=[]
+
+i=0
+quote=''
+
+	
+for x in ff:
+
+	# print(i)
+	# print(x)
+	
+	quote='" '+x+' "'
+	captions.append(quote)
+	quote=''
+		
+	i+=1
+
+pp.pprint(captions)
+
+
+f2=open(MAIN_TAG+'.csv','a+')
+
+for x in captions:
+
+	f2.write(str(ID)+',')
+	f2.write(x+',')
+	f2.write(MAIN_TAG+'\n')
+	ID+=1
+
+f2.close()
+
+print('')
